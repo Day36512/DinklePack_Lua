3a1db854007db30860e2f99992d39755bf9358d3
Dinkledork <118951051+Day36512@users.noreply.github.com>
2023-05-03 15:51:19 -0600

    Add files via upload

--- Custom_XP.lua
+++ Custom_XP.lua
@@ -1,5 +1,3 @@
--- .xp 0.01 - .xp 10
-
 local enabled = true -- set to false to disable the entire script
 local GMonly = false -- set to true to allow only GMs to change rates
 
@@ -17,7 +15,7 @@ local function OnLogin(event, player)
 
     if Q then
         local CharID, Rate = Q:GetUInt32(0), Q:GetFloat(1)
-        player:SendBroadcastMessage(string.format("|cff5af304Your XP rate is curently set to %.1f|r", Rate))
+        player:SendBroadcastMessage(string.format("|cff5af304Your experience rate is curently set to %.1f|r", Rate))
     end
 end
 
@@ -25,15 +23,29 @@ local function OnFirstLogin(event, player)
     local PUID = getPlayerCharacterGUID(player)
     local defaultRate = 1
     WorldDBExecute(string.format("INSERT INTO custom_xp VALUES (%i, %.2f)", PUID, defaultRate))
-    player:SendBroadcastMessage(string.format("|cff5af304Your XP rate is set to default: %.1fx|r", defaultRate))
+    player:SendBroadcastMessage(string.format("|cff5af304Your experience rate is set to default: %.1fx|r", defaultRate))
 end
 
 local function SetRate(event, player, command)
     local mingmrank = 3
     local PUID = getPlayerCharacterGUID(player)
 
-    if command:find("xp") then
-        local rate = tonumber(command:sub(4))
+    if command:find("xp") or command:find("exp") then
+        
+        if command:find("q") or command:find("Q") or command:find("?") or command == "xp" or command == "exp" then
+            local Q = WorldDBQuery(string.format("SELECT * FROM custom_xp WHERE CharID=%i", PUID))
+            if Q then
+                local CharID, Rate = Q:GetUInt32(0), Q:GetFloat(1)
+                player:SendBroadcastMessage(string.format("|cff5af304Your experience rate is currently set to %.1fx|r", Rate))
+            else
+                player:SendBroadcastMessage("|cff5af304Your experience rate is not found, default experience rate will be applied.|r")
+            end
+            
+            player:SendBroadcastMessage("|cff5af304To set your experience rate, type '.xp X' or '.exp X' where X is a value between 0.01 and 10.|r")
+            return false
+        end
+
+        local rate = tonumber(command:sub(command:find("xp") and 4 or 5))
 
         if rate and rate >= 0.01 and rate <= 10 then
             if (GMonly and player:GetGMRank() < mingmrank) then
@@ -42,13 +54,12 @@ local function SetRate(event, player, command)
             else
                 WorldDBExecute(string.format("DELETE FROM custom_xp WHERE CharID = %i", PUID))
                 WorldDBExecute(string.format("INSERT INTO custom_xp VALUES (%i, %.2f)", PUID, rate))
-                player:SendBroadcastMessage(string.format("|cff5af304You changed your XP rate to %.2fx|r", rate))
+                player:SendBroadcastMessage(string.format("|cff5af304You changed your experience rate to %.2fx|r", rate))
                 return false
             end
         end
     end
 end
-
 
 local function OnXP(event, player, amount, victim)
     local PUID = getPlayerCharacterGUID(player)
